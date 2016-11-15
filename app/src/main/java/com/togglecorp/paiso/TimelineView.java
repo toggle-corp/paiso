d2a04e918e5d5c064954ecc6a1ec5dde6c232c23
Frozen Helium <fren.ankit@gmail.com>
2016-11-15 14:41:41 +0545

    Sync

--- app/src/main/java/com/togglecorp/paiso/TimelineView.java
+++ app/src/main/java/com/togglecorp/paiso/TimelineView.java
@@ -2,17 +2,21 @@ package com.togglecorp.paiso;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.widget.ImageView;
+
+import java.util.Date;
 
 /**
  * Created by fhx on 11/14/16.
  */
 
 public class TimelineView extends ImageView {
-    private int mWidth, mHeight;
+    private Paint mBgPaint;
+    private Paint mLinePaint;
+
     public TimelineView(Context context) {
         super(context);
         this.prepare(context, null);
@@ -32,34 +36,47 @@ public class TimelineView extends ImageView {
         this.setFocusable(true);
         this.setScaleType(ScaleType.CENTER_INSIDE);
         this.setClickable(true);
-    }
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        mWidth = w;
-        mHeight = h;
-        super.onSizeChanged(w, h, oldw, oldh);
+        mBgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mBgPaint.setStyle(Paint.Style.FILL);
+        mBgPaint.setColor(ContextCompat.getColor(getContext(), R.color.colorPrimaryDark));
+
+        mLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mLinePaint.setStyle(Paint.Style.STROKE);
+        mLinePaint.setColor(ContextCompat.getColor(getContext(), R.color.colorAccent));
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        Paint bgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        bgPaint.setStyle(Paint.Style.FILL);
-        bgPaint.setColor(Color.parseColor("#212121"));
+        canvas.drawRect(0, 0, getWidth(), getHeight(), mBgPaint);
+        canvas.drawRect(getWidth()/2, 0, getWidth()/2, getHeight(), mLinePaint);
 
-        Paint fgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        fgPaint.setStyle(Paint.Style.STROKE);
-        fgPaint.setStrokeWidth(1);
-        fgPaint.setColor(Color.parseColor("#e04757"));
-
-        canvas.drawRect(0, 0, getWidth(), getHeight(), bgPaint);
-        canvas.drawRect(getWidth()/2, 0, getWidth()/2, getHeight(), fgPaint);
-
-        canvas.drawRect(getWidth()/2, 60, 50+getWidth()/2, 60, fgPaint);
-        canvas.drawRect(-50+getWidth()/2, 100, getWidth()/2, 100, fgPaint);
-        canvas.drawRect(-50+getWidth()/2, 260, getWidth()/2, 260, fgPaint);
-        canvas.drawRect(getWidth()/2, 360, 50+getWidth()/2, 360, fgPaint);
+        canvas.drawRect(getWidth()/2, 60, 50+getWidth()/2, 60, mLinePaint);
+        canvas.drawRect(-50+getWidth()/2, 100, getWidth()/2, 100, mLinePaint);
+        canvas.drawRect(-50+getWidth()/2, 260, getWidth()/2, 260, mLinePaint);
+        canvas.drawRect(getWidth()/2, 360, 50+getWidth()/2, 360, mLinePaint);
 
         super.onDraw(canvas);
+    }
+
+    public class TimeEvent{
+        public Date date;
+        public String title;
+        public long amount;
+        public String dpUrl;
+
+        public TimeEvent(){
+            date = new Date();
+            title = "Bla bla";
+            amount = 5000;
+        }
+
+        public TimeEvent(Date _date, String _title, long _amount, String _dpUrl){
+            date = _date;
+            title = _title;
+            amount = _amount;
+            dpUrl = _dpUrl;
+        }
     }
 }
