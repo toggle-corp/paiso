4c68cbecce55342df22a4af983d037edb39732a6
Bibek Dahal <bibekdahal.bd16@gmail.com>
2017-02-16 15:13:10 +0545

    Add home view

--- web/transactions/views.py
+++ web/transactions/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import View, TemplateView
 
-# Create your views here.
+
+
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'paiso/home.html')
